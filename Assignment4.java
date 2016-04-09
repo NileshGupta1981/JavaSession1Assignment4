781dd603a102d814e2538b04520fb607b0594a7e
NileshGupta1981 <nileshgupta1981@gmail.com>
2016-04-09 12:51:33 +0530

    Java Session1 Assignment 4

---
+++ Assignment4.java
@@ -0,0 +1,13 @@
+public class Assignment4{
+
+public static void main(String[] args){
+	
+	int x=5;
+	int y=10;
+System.out.println(x+y*2);
+System.out.println(x-y+2);
+System.out.println((x+y)*2);
+System.out.println(y%x);
+
+}
+}
